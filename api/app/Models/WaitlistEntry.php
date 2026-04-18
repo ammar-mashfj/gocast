2b41636da326fd260ee208c2714c1cd641ae052d
ammar-mashfj <ammarmashfj@live.com>
2026-04-18 14:54:14 +0300

    docs(waitlist): add @property phpdoc to WaitlistEntry

--- api/app/Models/WaitlistEntry.php
+++ api/app/Models/WaitlistEntry.php
@@ -3,7 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property int $id
+ * @property string $email
+ * @property string $plan
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
 class WaitlistEntry extends Model
 {
     protected $fillable = [
